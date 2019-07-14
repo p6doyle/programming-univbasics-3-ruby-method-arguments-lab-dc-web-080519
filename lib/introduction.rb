785f66ce20836ffc0efc4f05f9194343e3125b43
Peyton Doyle <p6doyle@gmail.com>
2019-07-14 11:34:52 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -14,10 +14,9 @@ end
 introduction_with_language(name,language)
 
 def introduction_with_language_optional(name,language)
-  if language(:age)
-    puts args[:age]
-  end
+  if language = Ruby
     puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  end
 end
 
 introduction_with_language_optional(name,language)
