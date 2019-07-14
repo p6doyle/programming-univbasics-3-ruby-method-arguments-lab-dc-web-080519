8770dd7a2ae7808c16dfb938f2c53e3457f6f7c6
Peyton Doyle <p6doyle@gmail.com>
2019-07-14 11:22:40 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,7 @@ end
 
 introduction(name)
 
-def introdution_with_language(name,language)
+def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
